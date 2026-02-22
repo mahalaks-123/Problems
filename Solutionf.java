5d95cc308ac0bc57225742d689a1da26e7afdc7a
mahalaks-123 <mahalakshmi21092006_bcs27@mepcoeng.ac.in>
2026-02-22 21:52:22 +0530

    Create Solutionf.java

---
+++ Solutionf.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solutionf {
+/*
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+     // Complete this line
+    // Complete this line
+
+    System.out.println(a);
+    // Complete this line
+    // Complete this line
+}*/
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    while (scan.hasNext()) {
+        System.out.println(scan.nextInt());
+    }
+}
+}
