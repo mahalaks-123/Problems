e406aadb972c8d2ea5c19bc59eab1f550f7515c0
mahalaks-123 <mahalakshmi21092006_bcs27@mepcoeng.ac.in>
2026-02-22 21:53:55 +0530

    Create Checknum.java

---
+++ Checknum.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Checknum { public static void main(String[] args) throws IOException { BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    bufferedReader.close();
+
+    if(n%2!=0) {
+        System.out.println("Weird");
+    } else if(n>=2 && n<=5 && n%2==0) {
+        System.out.println("Not Weird");
+    } else if(n>=6 && n<=20 && n%2==0) {
+        System.out.println("Weird");
+    } else if(n>20 && n%2==0) {
+        System.out.println("Not Weird");
+    } 
+}
+}
